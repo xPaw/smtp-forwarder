479e6b7558afe25d09fb674f54e70b2c63741f64
Pavel Djundik <xPaw@users.noreply.github.com>
2020-06-01 16:19:17 +0300

    Add example table

--- example_store.php
+++ example_store.php
@@ -13,6 +13,18 @@ $From = $Message->getHeaderValue( 'from' ); // from address
 $To = $Message->getHeaderValue( 'to' ); // to address
 
 /*
+
+CREATE TABLE `emails` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `Date` datetime NOT NULL DEFAULT current_timestamp(),
+  `Subject` text COLLATE utf8mb4_bin NOT NULL,
+  `AddressTo` varchar(256) CHARACTER SET ascii NOT NULL,
+  `AddressFrom` varchar(256) CHARACTER SET ascii NOT NULL,
+  `Mail` mediumtext COLLATE utf8mb4_bin NOT NULL,
+  PRIMARY KEY (`Id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+
 $InsertEmail = $Database->prepare(
 	'INSERT INTO `emails` (`Subject`, `AddressFrom`, `AddressTo`, `Mail`) ' .
 	'VALUES (:subject, :from, :to, :mail)'
